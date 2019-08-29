d9454ddc4ae0cdb329b3476aa2e48bd87f4df43a
Julian <surtr86@gmx.de>
2019-08-29 15:12:04 +0200

    minor modifications on dashboard html and css and weatherdata service

--- src/app/core/services/weatherdata.service.ts
+++ src/app/core/services/weatherdata.service.ts
@@ -23,11 +23,10 @@ export class WeatherdataService {
   getWeatherdata(): Observable<IWeatherdata[]> {
     return this.http.get("./assets/weatherdata.json")
         .map(res => {
-            console.log('fetched weatherdata')
             let data = res.json();
             return data;
         })
         .catch((err: Response) => Observable.of(err.json()));
     }
-
+    
 }
